288c8f42f00e5dd1fc0b1b2cd18bbe8bd6370910
Daniel Cordeiro <daniel.cordeiro@ladsistemas.com.br>
2020-07-28 15:32:48 -0300

    Review planos

--- src/main/java/com/dgc/util/EnumTipoPlano.java
+++ src/main/java/com/dgc/util/EnumTipoPlano.java
@@ -3,12 +3,12 @@ package com.dgc.util;
 public enum EnumTipoPlano {
 	SPTS("S/ Prancha Terça a Sexta", "SP TS", 30), 
 	SPTD("S/ Prancha Terça a Domingo", "SP TD", 30), 
-	SPTS90("S/ Prancha Terça a Sexta 60 dias", "SP TS 60", 60), 
-	SPTD90("S/ Prancha Terça a Domingo 60", "SP TD 60", 60), 
+	SPTS60("S/ Prancha Terça a Sexta 60 dias", "SP TS 60", 60), 
+	SPTD60("S/ Prancha Terça a Domingo 60", "SP TD 60", 60), 
 	CPTS("C/ Prancha Terça a Sexta", "CP TS", 30), 
 	CPTD("C/ Prancha Terça a Domingo", "CP TD", 30), 
-	CPTS90("C/ Prancha Terça a Sexta 60 dias", "CP TS 60", 60), 
-	CPTD90("C/ Prancha Terça a Domingo 60", "CP TD 60", 60),
+	CPTS60("C/ Prancha Terça a Sexta 60 dias", "CP TS 60", 60), 
+	CPTD60("C/ Prancha Terça a Domingo 60", "CP TD 60", 60),
 	SPTD0("S/ Prancha Terça a Domingo - sem limite", "SP TD0", 00);
 	
 	private String label;
